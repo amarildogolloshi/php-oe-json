8c85c087b6cea0794b1f043487a19776f884a479
Olivier Dony <odo@openerp.com>
2014-01-20 17:46:28 +0100

    [FIX] OpenERPException: make attributes public

--- openerp.php
+++ openerp.php
@@ -4,6 +4,9 @@ require_once 'tivoka/include.php';
 
 
 class OpenERPException extends Exception {
+    public $message;
+    public $code;
+    public $details;
     function __construct($message="", $code=0, $details=null) {
         parent::__construct($message, $code);
         $this->details = $details;
